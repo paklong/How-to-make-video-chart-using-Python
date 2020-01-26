f1ba52b7c255080dd1cd7276f01351fa0c30af5c
paklong <paklong2556@gmail.com>
2020-01-26 13:47:35 +0800

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
+#Data can be found here: https://ourworldindata.org/grapher/international-tourist-arrivals-by-world-region
 data = pd.read_csv('international-tourist-arrivals-by-world-region.csv', header = 0)
 data = data.rename(columns = {' (arrivals)': 'Arrivals'})
 Year = np.sort(data.Year.unique())
