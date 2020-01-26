882b7c72923e6df8eb83ddf01d97f8cd5cb74dd5
paklong <paklong2556@gmail.com>
2020-01-26 13:46:56 +0800

    Update code.py

--- code.py
+++ code.py
@@ -3,3 +3,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
+
+data = pd.read_csv('international-tourist-arrivals-by-world-region.csv', header = 0)
+data = data.rename(columns = {' (arrivals)': 'Arrivals'})
+Year = np.sort(data.Year.unique())
+
+for yr in Year:
+    Entity = data[data.Year == yr].sort_values('Arrivals', ascending = False).Entity
+    Arrivals = data[data.Year == yr].sort_values('Arrivals', ascending = False).Arrivals
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    Entity_pos = np.arange(len(Entity))
+
+    ax.barh(Entity_pos, Arrivals, align='center', color="blue")
+    ax.set_yticks(Entity_pos)
+    ax.set_yticklabels(Entity)
+    ax.invert_yaxis() 
+    ax.set_xlabel('Arrivals')
+    ax.set_title('International tourist arrivals by world region')
+
+
+    for i, v in enumerate(Arrivals):
+        ax.text(v, i, " "+str(v), va='center')
+        if i == 0:
+            ax.text(v , 4, " "+str(yr), weight="bold")
+
+
+    ax.set(frame_on=False)
+    plt.savefig(str(yr) + '.png', bbox_inches='tight')
+    
